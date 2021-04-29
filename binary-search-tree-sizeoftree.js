c353aeb635ad71a0f6d4d75b135f73eff4abbba2
Manish Bharti <manish2bharti@users.noreply.github.com>
2021-04-29 10:21:09 +0530

    Create binary-search-tree-sizeoftree.js

---
+++ binary-search-tree-sizeoftree.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(value) {
+        this.val = value;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+}
+
+class BinarySearchTree {
+    constructor(rootValue) {
+        this.root = new Node(rootValue);
+    }
+
+    insert(currentNode, newValue) {
+        if (currentNode === null) {
+            currentNode = new Node(newValue);
+        } else if (newValue < currentNode.val) {
+            currentNode.leftChild = this.insert(currentNode.leftChild, newValue);
+        } else {
+            currentNode.rightChild = this.insert(currentNode.rightChild, newValue);
+        }
+        return currentNode;
+    }
+  
+    insertBST(newValue) {
+        if(this.root==null){
+            this.root=new Node(newValue);
+            return;
+        }
+        this.insert(this.root, newValue);
+    }
+  
+    sizeOfTree(root){
+      if(root == null){
+            return 0;
+      }
+      return this.sizeOfTree(root.leftChild) + this.sizeOfTree(root.rightChild) + 1;
+    }
+
+
+}
+
+var BST = new BinarySearchTree(6);
+BST.insertBST(4);
+BST.insertBST(9);
+BST.insertBST(5);
+BST.insertBST(2);
+BST.insertBST(8);
+BST.insertBST(12);
+
+console.log(BST.sizeOfTree(BST.root))
